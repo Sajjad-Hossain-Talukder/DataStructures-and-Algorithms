bffcf21a9ef0d521270bb2a36406a9bdcad6a6f1
Sajjadhossaintalukder <63524824+Sajjadhossaintalukder@users.noreply.github.com>
2020-11-26 03:51:57 +0600

    Create isPrime?.cpp

---
+++ Number Theory/isPrime?.cpp
@@ -0,0 +1,27 @@
+//prime or notPrime
+
+
+
+//------------------
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main(){
+    ll n , fg = 0 ;
+    cin>>n;
+
+    for(int i = 2 ; i*i<=n ; i++){
+       if(n%i==0){
+            fg = 1;
+            break;
+       }
+    }
+    if(!fg) cout<<"Prime"<<endl;
+    else cout<<"notPrime"<<endl;
+
+return 0;
+}
