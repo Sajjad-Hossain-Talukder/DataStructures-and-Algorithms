9afec3c79df8f9d04b1c099f27357d80d2603686
Sajjadhossaintalukder <63524824+Sajjadhossaintalukder@users.noreply.github.com>
2020-11-26 03:45:32 +0600

    Create Divisor.cpp

---
+++ Number Theory/Divisor.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main(){
+    ll n , sum = 0 ;
+    cin>>n;
+    vector<int>divisor;
+
+    for(int i = 1 ; i*i<=n ; i++){
+        if(n%i==0){
+            divisor.pb(i);
+            sum+=i;
+            if(i*i!=n){
+                divisor.pb(n/i);
+                sum+=(n/i);
+            }
+        }
+    }
+    sort(divisor.begin() , divisor.end());
+    
+    // Divisor of a Number
+    for(auto a : divisor ) cout<<a<<" ";cout<<endl;
+    
+    //Number of divisor of a Number
+    cout<<divisor.size()<<endl;
+    
+    //Sum of Divisor of an number
+    cout<<sum<<endl;
+    
+return 0;
+}
