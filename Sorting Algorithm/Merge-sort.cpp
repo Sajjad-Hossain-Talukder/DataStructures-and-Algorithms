bbcf7ff8f4f84d6594a29dc3bb6967bf9cd00004
Sajjadhossaintalukder <63524824+Sajjadhossaintalukder@users.noreply.github.com>
2021-03-10 02:11:00 +0600

    Create Merge-sort.cpp

---
+++ Sorting Algorithm/Merge-sort.cpp
@@ -0,0 +1,58 @@
+// Source : https://www.youtube.com/watch?v=Nrr-lwS_0LY
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+void merging ( int *ar , int l , int mid , int h ) {
+
+    int i = l , j = mid+1 , k = 0 ;
+    int temp[h-l+1] ;
+
+    while( i <= mid && j <= h ){
+        if( ar[i] <= ar[j] )
+            temp[k++] = ar[i++] ;
+        else
+            temp[k++] = ar[j++];
+    }
+
+    while( i <= mid )  temp[k++] = ar[i++] ;
+    while( j <= h )    temp[k++] = ar[j++] ;
+    k = 0 ;
+
+    for(int p = l ; p <= h ; p++ )
+        ar[p] = temp[k++];
+
+
+}
+
+
+void mergesort ( int ar[] , int l , int  h ) {
+
+        int mid = (l+h) / 2 ;
+        if( l ==  h ) return ;
+
+        mergesort ( ar , l , mid ) ;
+        mergesort ( ar , mid+1 , h ) ;
+
+        merging( ar , l , mid , h ) ;
+
+}
+
+int main(){
+
+    int n ;
+    cin >> n;
+    int ar[n] ;
+    for( int i = 0 ; i < n ; i++ )
+        cin >> ar[i] ;
+
+
+    mergesort( ar, 0, n-1 ) ;
+
+    for( int i = 0 ; i < n ; i++ )
+        cout << ar[i] << " "; cout << endl;
+
+return 0;
+}
+
