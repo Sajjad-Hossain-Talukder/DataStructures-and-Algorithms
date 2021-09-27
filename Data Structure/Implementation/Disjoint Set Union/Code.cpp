41964a0e337a25e4805559a79f334fbe5ad3f810
Sajjad-Hossain-Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2021-09-28 02:11:54 +0600

    Update Code.cpp

--- Data Structure/Implementation/Disjoint Set Union/Code.cpp
+++ Data Structure/Implementation/Disjoint Set Union/Code.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 #define ll long long
 #define pb push_back
@@ -7,45 +8,25 @@
 #define mod 1000000007
 using namespace std;
 
+
 ll pathCompression ( ll parent[] , ll n ){
     if( parent[n] < 0 ) return n ;
     parent[n] = pathCompression(parent,parent[n]);
 }
 
 
+void DSU (ll parent[] , vector<pair<ll,pair<ll,ll>>>graph ){
 
-int main(){
-  
-    ll n , e , x , y , cost ; 
-    cin >> n >> e ; 
-
-    map < ll , pair<ll,ll>  >  tp ; 
-  
-    ll parent[n+1];
-    memset(parent,-1,sizeof(parent)); 
-
-
-    fr(i,0,e){
-        cin >> x >> y >> cost ; 
-        tp[cost] = {x,y};
-    }
-
-    for(auto a : tp ){
-        x = a.second.first ; 
-        y = a.second.second ; 
-        //cout << x <<" "<< y << endl;
+    for(auto a : graph ){
+        ll x = a.second.first ;
+        ll y = a.second.second ;
 
         while( parent[x] > 0 || parent[y] > 0 ){
-            if( parent[x] > 0 )
-                x = parent[x];
-            if( parent[y] > 0 )
-                y = parent[y]; 
+            if( parent[x] > 0 ) x = parent[x];
+            if( parent[y] > 0 ) y = parent[y];
         }
 
         if( x != y ) {
-            //cout << x <<" | "<< y << endl;
-            //cout << parent[x] <<" "<< parent[y] << endl;
-
             if( parent[x] <= parent[y]){
                 parent[x]+=parent[y] ;
                 parent[y] = x ;
@@ -54,17 +35,55 @@ int main(){
                 parent[y]+=parent[x] ;
                 parent[x] = y ;
             }
-            //cout << parent[x] <<" -- "<< parent[y] << endl;
-
         }
-
     }
 
+}
 
+
+
+int main(){
+
+    #ifndef CP
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll n , e , x , y , cost ;
+    cin >> n >> e ;
+
+    vector < pair<ll ,pair<ll,ll>>  > graph  ;
+
+    fr(i,0,e){
+        cin >> x >> y >> cost ;
+        graph.pb({cost,{x,y}});
+    }
+
+    sort(graph.begin(),graph.end());
+
+
+    ll parent[n+1];
+    memset(parent,-1,sizeof(parent));
+
+    DSU(parent,graph);
+
+
+    
     for(int i = 1 ; i <= n ; i++ )
         cout << parent[i] <<" " ; cout << endl;
-  
-  
+
+    for(int i = 1 ; i <= n ; i++ ){
+        pathCompression(parent,i);
+        cout << parent[i] <<" "; 
+    }
+    cout << endl;
+
+
+
+
+return 0 ;
+}
+
 /*
 
     Collapsing Find / Path Compression :  Making all the node with same parent here .
@@ -77,20 +96,15 @@ int main(){
        MST.
        This is known as Collapsing Find. It's also known as Path Compression .
 
-
-    */
-
-    
-
     for(int i = 1 ; i <= n ; i++ ){
-        pathCompression(parent,i);
+        x = i ;
+        if( parent[i] > 0 ){
+            while( parent[x] > 0 ) x = parent[x];
+            parent[i] = x;
+        }
     }
 
-
-
-    
-return 0 ;
-}
+    */
 
 /*
 8 9
@@ -98,9 +112,9 @@ return 0 ;
 1 3 7
 2 4 5
 2 5 6
-3 4 2 
+3 4 2
 5 6 3
-5 7 9 
+5 7 9
 6 8 8
 7 8 4
 
