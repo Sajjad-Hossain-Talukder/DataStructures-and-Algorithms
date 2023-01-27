21abc055effcf510b291658731036d296b5db01e
Sajjad-Hossain-Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-01-27 23:48:55 +0600

    Create templete.cpp

---
+++ templete.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define fast()      ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD         1000000007
+#define MOD1        998244353
+#define INF         1e18
+#define endl        "\n"
+#define pb          push_back
+#define ppb         pop_back
+#define mp          make_pair
+#define ff          first
+#define ss          second
+#define pii         pair<int,int>
+#define PI          3.141592653589793238462
+#define sz(x)       ((int)(x).size())
+#define all(x)      (x).begin(), (x).end()
+#define fr(i,s,e)   for(ll i=s;i<e;i++)
+#define rfr(i,e,s)  for(ll i=e;i>=s;i--)
+#define set_bits __builtin_popcountll
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    ll t ;
+    cin >> t ;
+}
