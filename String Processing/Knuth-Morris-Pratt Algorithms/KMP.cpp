f09336ef4e8a1d6a4b3cf9e10aba653918bf3f7d
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2025-02-07 12:00:29 +0600

    Update KMP.cpp

--- String Processing/Knuth-Morris-Pratt Algorithms/KMP.cpp
+++ String Processing/Knuth-Morris-Pratt Algorithms/KMP.cpp
@@ -1,60 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
-#define fr(i,s,e) for(ll i=s;i<e;i++)
-#define rfr(i,e,s) for(ll i=e;i>=s;i--)
-#define nl  "\n"
+#define fr(i, s, e) for (ll i = s; i < e; i++)
+#define rfr(i, e, s) for (ll i = e; i >= s; i--)
+#define nl "\n"
 #define mod 1000000007
 using namespace std;
 
-void patArray ( string s , vector <ll>&v){
-    ll j = 0 , d = s.size();
-    fr(i,1,d){
-        if(s[i] == s[j] )v.pb(++j);
-        else {
-            --j;
-            while(1){
-                if(j<0){  v.pb(0) ; j = 0 ; break; }
-                j = v[j];
-                if(s[j] == s[i] ){ v.pb(++j); break; }
-                j-=1;
+vector<int> createPatternArray(string pattern)
+{
+    int n = pattern.size(), i = 1, j = 0;
+    vector<int> lps(n, 0);
+    while (i < n){
+        if (pattern[i] == pattern[j]){
+            j++;
+            lps[i] = j;
+            i++;
+        }
+        else{
+            if (j != 0){
+                j = lps[j - 1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
             }
         }
     }
+    return lps;
 }
 
-void KMP ( string t , string p , vector <ll>v , vector<ll>& pos  ){
+vector<int> KMP(string text, string pattern){
+    std::vector<int> matches;
+    if (pattern.empty())
+        return matches;
 
-    ll j = 0 , d1 = t.size() , d2 = p.size() ;
-    fr(i,0,d1){
-        if ( t[i] == p[j] ) ++j;
-        else {
-            if(j>0) {
-                j = v[j-1];
-                --i;
+    vector<int> lps = createPatternArray(pattern);
+
+    int i = 0, j = 0;
+
+    while (i < text.length()){
+        if (pattern[j] == text[i]){
+            i++;
+            j++;
+        }
+
+        if (j == pattern.length()){
+            matches.push_back(i - j);
+            j = lps[j - 1];
+        }
+        else if (i < text.length() && pattern[j] != text[i]){
+            if (j != 0){
+                j = lps[j - 1];
+            }
+            else {
+                i++;
             }
         }
-        if( j == d2 ) { j = v[j-1] ; pos.pb(i-d2+1); }
     }
+    return matches;
 }
-
 
 int main(){
-    string text , pattern ;
-    cin >> text >> pattern ;
-    vector < ll > v , pos ;
+    string text = "abxabcdabxabcdabcdabcy", pattern = "abcdabcy";
+    // cin >> text >> pattern ;
+    vector<int> pos;
 
-    v.pb(0);
-    patArray(pattern,v);
-    KMP(text,pattern,v,pos);
+    vector<int> matches = KMP(text, pattern);
 
-    cout << "Total Equal Pattern : " << pos.size() << endl;
-    cout << "starting Position : ";
-    for( auto a : pos)
-        cout << a <<" "; cout << endl;
+    cout << "Found Pattern : " << matches.size() << endl;
+    cout << "Matched Positions : ";
+    for (auto a : matches)
+        cout << a << " ";
+    cout << endl;
 
-
-return 0 ;
+    return 0;
 }
-
-
